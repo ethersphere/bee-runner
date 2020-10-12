80cca557ec316a4ed36b468326b90510b8085845
Ivan Vandot <ivan@vandot.rs>
2020-10-12 20:46:49 +0200

    fix repeating actions

--- index.js
+++ index.js
@@ -5,22 +5,26 @@
 
 const commands = require('probot-commands')
 
-module.exports = async robot => {
+module.exports = robot => {
   robot.log.info('Yay, the robot was loaded!')
-  robot.on('issue_comment.created', async context => {
-    robot.log.info(context.payload.comment.author_association)
-    if ((context.payload.comment.author_association === "OWNER") || (context.payload.comment.author_association === "MEMBER")) {
-      commands(robot, 'label', async (context, command) => {
+    commands(robot, 'label', (context, command) => {
+      if ((context.payload.comment.author_association === "OWNER") || (context.payload.comment.author_association === "MEMBER")) {
         const labels = command.arguments.split(/, */);
         return context.github.issues.addLabels(context.issue({labels}));
-      });
+      }
+      return;
+    });
 
-      commands(robot, 'unlabel', async (context, command) => {
+    commands(robot, 'unlabel', (context, command) => {
+      if ((context.payload.comment.author_association === "OWNER") || (context.payload.comment.author_association === "MEMBER")) {
         const name = command.arguments;
         return context.github.issues.removeLabel(context.issue({name}));
-      });
+      }
+      return;
+    });
 
-      commands(robot, 'run', async (context, command) => {
+    commands(robot, 'run', async (context, command) => {
+      if ((context.payload.comment.author_association === "OWNER") || (context.payload.comment.author_association === "MEMBER")) {
         const event_type = command.arguments;
         const pr = context.pullRequest();
         const owner = pr.owner;
@@ -28,9 +32,8 @@ module.exports = async robot => {
 
         const pull = await context.github.pulls.get(pr);
         const client_payload = {"ref": pull.data.head.ref, "sha": pull.data.head.sha};
-
         return context.github.repos.createDispatchEvent({owner, repo, event_type, client_payload});
-      });
-    };
-  });
+      }
+      return;
+    });
 }
