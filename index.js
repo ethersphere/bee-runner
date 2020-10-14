bdf6151ec1b4becf66bd1d05fd7a735da03df41a
Nemanja Zbiljić <nemanja.zbiljic@gmail.com>
2020-10-14 16:30:41 +0200

    Check if commenting user is org member

--- index.js
+++ index.js
@@ -33,6 +33,28 @@ module.exports = robot => {
         const pull = await context.github.pulls.get(pr);
         const client_payload = {"ref": pull.data.head.ref, "sha": pull.data.head.sha};
         return context.github.repos.createDispatchEvent({owner, repo, event_type, client_payload});
+      } else if ((context.payload.comment.author_association === "COLLABORATOR") || (context.payload.comment.author_association === "CONTRIBUTOR")) {
+        // check if user belongs to project organization
+        const username = context.payload.comment.user.login;
+
+        if (!username) {
+          return;
+        }
+
+        const pr = context.pullRequest();
+        const owner = pr.owner;
+        const repo = pr.repo;
+
+        const response = await context.github.checkMembershipForUser({ org: owner, username });
+
+        if (response && response.status === 204) {
+          const event_type = command.arguments;
+
+          const pull = await context.github.pulls.get(pr);
+          const client_payload = { "ref": pull.data.head.ref, "sha": pull.data.head.sha };
+
+          return context.github.repos.createDispatchEvent({owner, repo, event_type, client_payload});
+        }
       }
       return;
     });
